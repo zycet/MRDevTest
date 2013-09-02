405d59270a68e20b308dbb16e8db54d5b71c5a15
张以成 <zycet@126.com>
2013-09-02 17:14:46 +0800

    第一个测试程序

---
+++ src/MRDevTest.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MRDevTest.cpp
+// Author      : ZYC
+// Version     :
+// Copyright   : BUAA
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
